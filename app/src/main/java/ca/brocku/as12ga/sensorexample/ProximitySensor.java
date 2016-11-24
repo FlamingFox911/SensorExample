5738af7087acfe93a15ed3e69fdcb00109ca5979
FlamingFox <flaming_fox911@hotmail.com>
2016-11-24 16:11:10 -0500

    Final Commit for Demo App.

---
+++ app/src/main/java/ca/brocku/as12ga/sensorexample/ProximitySensor.java
@@ -0,0 +1,72 @@
+package ca.brocku.as12ga.sensorexample;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ProximitySensor extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager mSensorManager;
+    Sensor mProximity;
+    TextView display;
+    ProgressBar distance;
+    float max;
+
+    // When we create the app.
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_proximity_sensor);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        display = (TextView) findViewById(R.id.sensorDisplay);
+        distance = (ProgressBar) findViewById(R.id.distanceBar);
+
+        max = mProximity.getMaximumRange();
+        distance.setMax((int)max);
+    }
+
+    // When we resume the app.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    // When we pause the app.
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    // Triggers when ANY sensor has detected a change, depending on it's min/max delay.
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            float val = event.values[0];
+            String out = "Proximity Value: " + val + "\n\nThe Proximity Sensor only recognizes " +
+                    "near or far states, even though we get these value.\nSo, we can instead " +
+                    "display if we have 'triggered' the sensor or not, via the progress bar.\n";
+            display.setText(out);
+            distance.setProgress((int)val);
+        }
+    }
+
+    // Triggers when the accuracy of a sensor changes (such as GPS)
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Used only if we want to do something regarding precision.
+        // Since we implemented SensorEventListener, we must include this,
+        // even though we don't need it.
+    }
+
+}
+
--- app/src/main/java/ca/brocku/as12ga/sensorexample/SensorActivity.java
+++ app/src/main/java/ca/brocku/as12ga/sensorexample/SensorActivity.java
@@ -1,10 +1,12 @@
 package ca.brocku.as12ga.sensorexample;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,6 +19,7 @@ public class SensorActivity extends AppCompatActivity {
     int numSensors;
     String[] sensorTypes;
 
+    // When we create the app.
     public void onCreate(Bundle b) {
         super.onCreate(b);
         setContentView(R.layout.activity_sensor);
@@ -32,7 +35,17 @@ public class SensorActivity extends AppCompatActivity {
 
         total.append("Number of Sensors: " + numSensors);
         for(int i = 0; i < numSensors; i++) {
-            list.append("Name: " + listSensor.get(i).getName() + "\nType: " + sensorTypes[listSensor.get(i).getType()] + "\nBrand: " + listSensor.get(i).getVendor() + "\n\n");
+            list.append("Name: " + listSensor.get(i).getName() + "\nType: " + sensorTypes[listSensor.get(i).getType()] + "\nBrand: " + listSensor.get(i).getVendor());
+            if (i < numSensors - 1){
+                list.append("\n\n");
+            }
         }
     }
+
+    // The intent function to "proceed" to the next activity.
+    public void proceed(View view) {
+        Intent intent = new Intent(this, ProximitySensor.class);
+        startActivity(intent);
+    }
+
 }
